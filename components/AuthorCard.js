f5395d82e7d9c3fd03e8e9d64753a152fac5011e
Kristen Chandler <kristenchandler@kristens-mbp.lan>
2022-07-18 20:06:23 -0500

    got author cards displaying on DOM

--- components/AuthorCard.js
+++ components/AuthorCard.js
@@ -1,38 +1,60 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import deleteAuthor from '../api/authorData';
 
 function AuthorCard({
-  firstName, lastName, email, firebaseKey, favorite, image,
+  onUpdate, authorObj,
 }) {
+  const deleteThisAuthor = () => {
+    if (window.confirm(`Delete ${authorObj.first_name}?`)) {
+      deleteAuthor(authorObj.firebaseKey).then(() => onUpdate());
+    }
+  };
   return (
-    <>
-      <img src={image} alt={firstName} />
-      <div>First Name: {firstName}</div>
-      <div>Last Name: {lastName}</div>
-      <div>Email: {email}</div>
-      <div>Favorite: {favorite}</div>
-      <div>firebaseKey: {firebaseKey}</div>
-    </>
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Body>
+        <Card.Title>{authorObj.first_name} {authorObj.last_name}</Card.Title>
+        <p className="card-text bold">{authorObj.favorite && <span>FAVORITE<br /></span> }</p>
+        <p>{authorObj.email}</p>
+        {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
+        <Link href={`/book/${authorObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <Link href={`/book/edit/${authorObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisAuthor} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
   );
 }
 
 AuthorCard.propTypes = {
-  image: PropTypes.string,
-  firstName: PropTypes.string,
-  lastName: PropTypes.string,
-  favorite: PropTypes.bool,
-  email: PropTypes.string,
-  firebaseKey: PropTypes.string,
+  authorObj: PropTypes.shape({
+    image: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    favorite: PropTypes.bool,
+    email: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
-AuthorCard.defaultProps = {
-  image: 'https://i.pinimg.com/564x/46/8c/71/468c7183d4c0ff5c0523f34002610a94.jpg',
-  firstName: 'Eleven',
-  lastName: 'Hopper',
-  email: '11@upsidedown.com',
-  favorite: false,
-  firebaseKey: '011',
-};
+// AuthorCard.defaultProps = {
+//   image: 'https://i.pinimg.com/564x/46/8c/71/468c7183d4c0ff5c0523f34002610a94.jpg',
+//   firstName: 'Eleven',
+//   lastName: 'Hopper',
+//   email: '11@upsidedown.com',
+//   favorite: false,
+//   firebaseKey: '011',
+// };
 
 export default AuthorCard;
--- pages/authors.js
+++ pages/authors.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
+import { getAuthors } from '../api/authorData';
 import AuthorCard from '../components/AuthorCard';
+import { useAuth } from '../utils/context/authContext';
 
 export default function Authors() {
+  const [authors, setAuthors] = useState([]);
+  const { user } = useAuth();
+  useEffect(() => {
+    getAuthors(user.uid).then(setAuthors);
+  }, [user.uid]);
   return (
-    <>
-      <AuthorCard />
-    </>
+    <div className="text-center my-4">
+      <Link href="/author/new" passHref>
+        <Button>Add An Author</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {authors.map((author) => (
+          <AuthorCard key={author.firebaseKey} authorObj={author} />
+        ))}
+      </div>
+    </div>
   );
 }
